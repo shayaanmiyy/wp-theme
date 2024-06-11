53cc85973f8af3c775ab5c82ccaf06c8d83c483f
Shayaan <shayaanmomin.sm@gmail.com>
2024-06-11 20:38:36 +0530

    first commit

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+// Load Timber library
+if (!class_exists('Timber')) {
+    add_action('admin_notices', function() {
+        echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber-library')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
+    });
+    return;
+}
+
+Timber::$dirname = array('templates', 'views');
+
+class MySite extends Timber\Site {
+    public function __construct() {
+        add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
+        add_theme_support('post-thumbnails');
+        add_theme_support('menus');
+        add_filter('timber/context', array($this, 'add_to_context'));
+        add_filter('timber/twig', array($this, 'add_to_twig'));
+        parent::__construct();
+    }
+
+    public function add_to_context($context) {
+        $context['foo'] = 'bar';
+        return $context;
+    }
+
+    public function add_to_twig($twig) {
+        $twig->addExtension(new Twig\Extension\StringLoaderExtension());
+        return $twig;
+    }
+}
+
+new MySite();
+
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// Load Timber library
+$context = Timber::context();
+Timber::render('index.twig', $context);
